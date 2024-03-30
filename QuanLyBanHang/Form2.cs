38a10d7a3465044f7047d8a3108a9f7d48127d73
Ho Hong Yen <yenvt9a1@gmail.com>
2024-03-30 22:28:35 +0700

    Fix FR003 – Đăng nhập

--- QuanLyBanHang/Form2.cs
+++ QuanLyBanHang/Form2.cs
@@ -23,7 +23,7 @@ namespace QuanLyBanHang
                 this.Close();
             else
             {
-                MessageBox.Show("Không đúng tên người dùng ? mật khẩu!!!", "Thông báo");
+                MessageBox.Show("Không đúng tên người dùng / mật khẩu!!!", "Thông báo");
                 this.txtUser.Focus();
             }
         }
@@ -45,5 +45,30 @@ namespace QuanLyBanHang
         {
 
         }
+
+        // insert begin
+        public bool ClosedByXButtonOrAltF4 { get; private set; }
+        private const int SC_CLOSE = 0xF060;
+        private const int WM_SYSCOMMAND = 0x0112;
+        protected override void WndProc(ref Message msg)
+        {
+            if (msg.Msg == WM_SYSCOMMAND && msg.WParam.ToInt32() == SC_CLOSE)
+                ClosedByXButtonOrAltF4 = true;
+            base.WndProc(ref msg);
+        }
+        protected override void OnShown(EventArgs e)
+        {
+            ClosedByXButtonOrAltF4 = false;
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (ClosedByXButtonOrAltF4)
+                //MessageBox.Show("Closed by X or Alt+F4");
+                Application.Exit();
+            //else
+                //MessageBox.Show("Closed by calling Close()");
+        }
+
+        // insert end
     }
 }
