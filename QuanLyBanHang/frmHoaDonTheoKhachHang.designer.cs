b1d3016a245a39889c1038523469e665c35cadae
Ho Hong Yen <yenvt9a1@gmail.com>
2024-03-31 09:57:35 +0700

    Fix FR019 - Quản lý Hóa đơn theo Khách hàng (đổi title của form)

--- QuanLyBanHang/frmHoaDonTheoKhachHang.designer.cs
+++ QuanLyBanHang/frmHoaDonTheoKhachHang.designer.cs
@@ -30,26 +30,27 @@
         {
             this.btnThoat = new System.Windows.Forms.Button();
             this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+            this.MaHD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaKH = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.MaNV = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.NgayLapHD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgayNhanHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.txtTongSoHD = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.pnlThongTinKH = new System.Windows.Forms.Panel();
             this.btnOK = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
             this.cbMaKH = new System.Windows.Forms.ComboBox();
-            this.MaHD = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.MaKH = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.MaNV = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.NgayLapHD = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NgayNhanHang = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
             this.pnlThongTinKH.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnThoat
             // 
-            this.btnThoat.Location = new System.Drawing.Point(653, 269);
+            this.btnThoat.Location = new System.Drawing.Point(871, 331);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.Size = new System.Drawing.Size(100, 28);
             this.btnThoat.TabIndex = 0;
             this.btnThoat.Text = "Trở về";
             this.btnThoat.UseVisualStyleBackColor = true;
@@ -57,6 +58,7 @@
             // 
             // dgvHoaDon
             // 
+            this.dgvHoaDon.AccessibleName = "";
             this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvHoaDon.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.MaHD,
@@ -64,24 +66,70 @@
             this.MaNV,
             this.NgayLapHD,
             this.NgayNhanHang});
-            this.dgvHoaDon.Location = new System.Drawing.Point(12, 44);
+            this.dgvHoaDon.Location = new System.Drawing.Point(16, 54);
+            this.dgvHoaDon.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvHoaDon.Name = "dgvHoaDon";
-            this.dgvHoaDon.Size = new System.Drawing.Size(716, 203);
+            this.dgvHoaDon.RowHeadersWidth = 51;
+            this.dgvHoaDon.Size = new System.Drawing.Size(955, 250);
             this.dgvHoaDon.TabIndex = 1;
+            // 
+            // MaHD
+            // 
+            this.MaHD.DataPropertyName = "MaHD";
+            this.MaHD.HeaderText = "Mã HĐ";
+            this.MaHD.MinimumWidth = 6;
+            this.MaHD.Name = "MaHD";
+            this.MaHD.Width = 125;
+            // 
+            // MaKH
+            // 
+            this.MaKH.DataPropertyName = "MaKH";
+            this.MaKH.HeaderText = "Mã KH";
+            this.MaKH.MinimumWidth = 6;
+            this.MaKH.Name = "MaKH";
+            this.MaKH.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.MaKH.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.MaKH.Width = 125;
+            // 
+            // MaNV
+            // 
+            this.MaNV.DataPropertyName = "MaNV";
+            this.MaNV.HeaderText = "Mã NV";
+            this.MaNV.MinimumWidth = 6;
+            this.MaNV.Name = "MaNV";
+            this.MaNV.Width = 125;
+            // 
+            // NgayLapHD
+            // 
+            this.NgayLapHD.DataPropertyName = "NgayLapHD";
+            this.NgayLapHD.HeaderText = "Ngày Lập HĐ";
+            this.NgayLapHD.MinimumWidth = 6;
+            this.NgayLapHD.Name = "NgayLapHD";
+            this.NgayLapHD.Width = 125;
+            // 
+            // NgayNhanHang
+            // 
+            this.NgayNhanHang.DataPropertyName = "NgayNhanHang";
+            this.NgayNhanHang.HeaderText = "Ngày Nhận Hàng";
+            this.NgayNhanHang.MinimumWidth = 6;
+            this.NgayNhanHang.Name = "NgayNhanHang";
+            this.NgayNhanHang.Width = 125;
             // 
             // txtTongSoHD
             // 
-            this.txtTongSoHD.Location = new System.Drawing.Point(616, 0);
+            this.txtTongSoHD.Location = new System.Drawing.Point(821, 0);
+            this.txtTongSoHD.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtTongSoHD.Name = "txtTongSoHD";
-            this.txtTongSoHD.Size = new System.Drawing.Size(83, 20);
+            this.txtTongSoHD.Size = new System.Drawing.Size(109, 22);
             this.txtTongSoHD.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(533, 4);
+            this.label5.Location = new System.Drawing.Point(711, 5);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.Size = new System.Drawing.Size(82, 16);
             this.label5.TabIndex = 13;
             this.label5.Text = "Tổng số HĐ:";
             // 
@@ -92,16 +140,18 @@
             this.pnlThongTinKH.Controls.Add(this.label5);
             this.pnlThongTinKH.Controls.Add(this.cbMaKH);
             this.pnlThongTinKH.Controls.Add(this.txtTongSoHD);
-            this.pnlThongTinKH.Location = new System.Drawing.Point(12, 12);
+            this.pnlThongTinKH.Location = new System.Drawing.Point(16, 15);
+            this.pnlThongTinKH.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlThongTinKH.Name = "pnlThongTinKH";
-            this.pnlThongTinKH.Size = new System.Drawing.Size(716, 26);
+            this.pnlThongTinKH.Size = new System.Drawing.Size(955, 32);
             this.pnlThongTinKH.TabIndex = 14;
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(430, 1);
+            this.btnOK.Location = new System.Drawing.Point(573, 1);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(53, 23);
+            this.btnOK.Size = new System.Drawing.Size(71, 28);
             this.btnOK.TabIndex = 19;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -110,62 +160,33 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(20, 4);
+            this.label4.Location = new System.Drawing.Point(27, 5);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.Size = new System.Drawing.Size(114, 16);
             this.label4.TabIndex = 18;
             this.label4.Text = "Chọn Khách hàng:";
             // 
             // cbMaKH
             // 
             this.cbMaKH.FormattingEnabled = true;
-            this.cbMaKH.Location = new System.Drawing.Point(121, 1);
+            this.cbMaKH.Location = new System.Drawing.Point(161, 1);
+            this.cbMaKH.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cbMaKH.Name = "cbMaKH";
-            this.cbMaKH.Size = new System.Drawing.Size(303, 21);
+            this.cbMaKH.Size = new System.Drawing.Size(403, 24);
             this.cbMaKH.TabIndex = 17;
-            // 
-            // MaHD
-            // 
-            this.MaHD.DataPropertyName = "MaHD";
-            this.MaHD.HeaderText = "Mã HĐ";
-            this.MaHD.Name = "MaHD";
-            // 
-            // MaKH
-            // 
-            this.MaKH.DataPropertyName = "MaKH";
-            this.MaKH.HeaderText = "Mã KH";
-            this.MaKH.Name = "MaKH";
-            this.MaKH.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.MaKH.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
-            // 
-            // MaNV
-            // 
-            this.MaNV.DataPropertyName = "MaNV";
-            this.MaNV.HeaderText = "Mã NV";
-            this.MaNV.Name = "MaNV";
-            // 
-            // NgayLapHD
-            // 
-            this.NgayLapHD.DataPropertyName = "NgayLapHD";
-            this.NgayLapHD.HeaderText = "Ngày Lập HĐ";
-            this.NgayLapHD.Name = "NgayLapHD";
-            // 
-            // NgayNhanHang
-            // 
-            this.NgayNhanHang.DataPropertyName = "NgayNhanHang";
-            this.NgayNhanHang.HeaderText = "Ngày Nhận Hàng";
-            this.NgayNhanHang.Name = "NgayNhanHang";
             // 
             // frmHoaDonTheoKhachHang
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(746, 302);
+            this.ClientSize = new System.Drawing.Size(995, 372);
             this.Controls.Add(this.pnlThongTinKH);
             this.Controls.Add(this.dgvHoaDon);
             this.Controls.Add(this.btnThoat);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmHoaDonTheoKhachHang";
-            this.Text = "Quản lý  Khách hàng theo Thành phố";
+            this.Text = "Quản lý Hóa đơn theo Khách hàng";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmHoaDonTheoKhachHang_FormClosing);
             this.Load += new System.EventHandler(this.frmHoaDonTheoKhachHang_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
